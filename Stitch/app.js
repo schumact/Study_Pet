8dc0183743cf0ffb5a9deed9faff5bde3308b1c2
unknown <schumact@mail.uc.edu>
2020-01-01 17:33:12 -0500

    started incorporating stitch

---
+++ Stitch/app.js
@@ -0,0 +1,12 @@
+import {Stitch} from "mongodb-stitch-browser-sdk";
+
+
+// TODO: Add your Stitch app's App ID
+const APP_ID = "persistant_pet-koezb";
+
+// TODO: Initialize the app client
+const app = Stitch.hasAppClient(APP_ID)
+  ? Stitch.getAppClient(APP_ID)
+  : Stitch.initializeAppClient(APP_ID);
+
+export { app };
