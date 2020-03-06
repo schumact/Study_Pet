adf89e061dae22afd641c5064c7ff5ac380ed8c5
unknown <schumact@mail.uc.edu>
2020-03-06 14:20:48 -0500

    ignore

--- src/Stitch/StitchApp.ts
+++ src/Stitch/StitchApp.ts
@@ -1,11 +1,10 @@
-//import { Stitch } from "mongodb-stitch-react-native-sdk";
-import { Stitch } from "mongodb-stitch-browser-sdk";
+import { Stitch, StitchAppClient } from "mongodb-stitch-browser-sdk";
 
 // TODO: Add your Stitch app's App ID
 const APP_ID:string = "persistant_pet-koezb";
 
 // TODO: Initialize the app client
-const stitchApp:any = Stitch.hasAppClient(APP_ID)
+const stitchApp:StitchAppClient = Stitch.hasAppClient(APP_ID)
   ? Stitch.getAppClient(APP_ID)
   : Stitch.initializeAppClient(APP_ID);
 
